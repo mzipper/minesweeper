77477273a68a1f3c25f7b1d5a31a4fe987a3b082
Mark Zipper <37257954+mzipper@users.noreply.github.com>
2022-09-06 21:10:05 -0400

    code cleanup - reorganize lines, delete unused field, newline spacing

--- src/view/Window.java
+++ src/view/Window.java
@@ -6,19 +6,13 @@ import javax.swing.JFrame;
 
 public class Window extends JFrame {
     
-
-    IntListener intListener;
-    
     public FlagCountPanel flagCountPanel;
     public BoardPanel boardPanel;
 
 	private int numberofBombs = 10;
 
     public Window() {
-    	
 		initializeComponents();
-		
-		
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -27,33 +21,22 @@ public class Window extends JFrame {
 		
 		setLocationRelativeTo(null);
 		setVisible(true);
-    	
 	}
 
 	private void initializeComponents() {
 		
-		
-		
 		boardPanel = new BoardPanel(new IntListener() {
-			
 			@Override
 			public void numberEmitted(int number) {
 				flagCountPanel.flagCountLabel.setText(Integer.toString(number));
 			}
 		});
+		flagCountPanel = new FlagCountPanel(numberofBombs);
 		
 		setLayout(new BorderLayout());
 		
-		flagCountPanel = new FlagCountPanel(numberofBombs);
-		
-		
 		add(flagCountPanel, BorderLayout.NORTH);
 		add(boardPanel, BorderLayout.CENTER);
-		
 	}
-    
-    
-    
-    
-    
+   
 }
